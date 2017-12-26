65e12b76a773135d3caaff0528334fdc65898599
lihui1993 <1071252269@qq.com>
2017-12-26 16:34:25 +0800

    第一个自己整合的springboot的小项目
    在自己的努力下整合的springboot的项目，期间遇到了很多bug，在看springboot实战这本书没有提到bug都被我遇到了和解决了

---
+++ springboot-two-test/springboot-two-test-web/src/main/java/cn/allcheer/springbootbylihui/springboottwotestweb/SpringbootTwoTestWebApplication.java
@@ -0,0 +1,12 @@
+package cn.allcheer.springbootbylihui.springboottwotestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTwoTestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTwoTestWebApplication.class, args);
+	}
+}
