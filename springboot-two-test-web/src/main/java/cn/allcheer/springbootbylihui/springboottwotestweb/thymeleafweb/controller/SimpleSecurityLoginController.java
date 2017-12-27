464b8794878cf07163715406c107fa454bce0b1e
lihui <lihui@ref.cn>
2017-12-27 12:06:12 +0800

    改动返回页面/index变成index，不然thymeleaf模板找不到/index视图

--- springboot-two-test-web/src/main/java/cn/allcheer/springbootbylihui/springboottwotestweb/thymeleafweb/controller/SimpleSecurityLoginController.java
+++ springboot-two-test-web/src/main/java/cn/allcheer/springbootbylihui/springboottwotestweb/thymeleafweb/controller/SimpleSecurityLoginController.java
@@ -1,16 +1,19 @@
 package cn.allcheer.springbootbylihui.springboottwotestweb.thymeleafweb.controller;
 
 import cn.allcheer.springbootbylihui.springboottwotestdal.domain.model.Msg;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class SimpleSecurityLoginController {
     @RequestMapping("/")
     public String index(Model model){
         Msg msg=new Msg("测试标题","测试内容","额外信息，只对管理员显示");
         model.addAttribute("msg",msg);
-        return "/index";
+        log.debug("··········访问到了，给了model，返回index页面···");
+        return "index";
     }
 }
