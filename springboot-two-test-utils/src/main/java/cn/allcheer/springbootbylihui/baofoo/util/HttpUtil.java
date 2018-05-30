293d620fda4ff04ad42e9112d41bb1804ac18125
lihui <lihui@ref.cn>
2018-05-30 15:43:38 +0800

    完善工具类

--- springboot-two-test-utils/src/main/java/cn/allcheer/springbootbylihui/baofoo/util/HttpUtil.java
+++ springboot-two-test-utils/src/main/java/cn/allcheer/springbootbylihui/baofoo/util/HttpUtil.java
@@ -14,147 +14,150 @@ import java.util.Map;
 
 
 import cn.allcheer.springbootbylihui.baofoo.http.*;
-import com.sun.deploy.net.HttpResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.omg.CORBA.NameValuePair;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 @Slf4j
 public class HttpUtil{
-	
-	public static String RequestForm(String Url,Map<String,String> Parms){		
-		if(Parms.isEmpty()){
-			return  "参数不能为空！";
-		}
-		String PostParms = "";
-		int PostItemTotal = Parms.keySet().size();
-		int Itemp=0;
-		for (String key : Parms.keySet()){
-			PostParms += key + "="+Parms.get(key);
-			Itemp++;
-			if(Itemp<PostItemTotal){
-				PostParms +="&";
-			}
-		}
-		HttpSendModel httpSendModel = new HttpSendModel(Url + "?" + PostParms);
-		log.info("【请求全部参数】：" + Url + "?" + PostParms);
-		httpSendModel.setMethod(HttpMethod.POST);
-		SimpleHttpResponse response = null;
-		try {
-			response = doRequest(httpSendModel, "utf-8");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return e.getMessage();
-		}
-		return response.getEntityString();
 
-	}
-	
-	public static SimpleHttpResponse doRequest(HttpSendModel httpSendModel,
-			String getCharSet) throws Exception {
+    public static String RequestForm(String Url,Map<String,String> Parms){
+        if(Parms.isEmpty()){
+            return  "参数不能为空！";
+        }
+        String PostParms = "";
+        int PostItemTotal = Parms.keySet().size();
+        int Itemp=0;
+        for (String key : Parms.keySet()){
+            PostParms += key + "="+Parms.get(key);
+            Itemp++;
+            if(Itemp<PostItemTotal){
+                PostParms +="&";
+            }
+        }
+        HttpSendModel httpSendModel = new HttpSendModel(Url + "?" + PostParms);
+        log.info("【请求全部参数】：" + Url + "?" + PostParms);
+        httpSendModel.setMethod(HttpMethod.POST);
+        SimpleHttpResponse response = null;
+        try {
+            response = doRequest(httpSendModel, "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        return response.getEntityString();
 
-		// 创建默认的httpClient客户端端
-		SimpleHttpClient simpleHttpclient = new SimpleHttpClient();
+    }
 
-		try {
-			return doRequest(simpleHttpclient, httpSendModel, getCharSet);
-		} finally {
-			simpleHttpclient.getHttpclient().getConnectionManager().shutdown();
-		}
+    public static SimpleHttpResponse doRequest(HttpSendModel httpSendModel,
+                                               String getCharSet) throws Exception {
 
-	}
+        // 创建默认的httpClient客户端端
+        SimpleHttpClient simpleHttpclient = new SimpleHttpClient();
 
-	/**
-	 * @param httpSendModel
-	 * @param getCharSet
-	 * @return
-	 * @throws Exception 
-	 */
-	public static SimpleHttpResponse doRequest(
-			SimpleHttpClient simpleHttpclient, HttpSendModel httpSendModel,
-			String getCharSet) throws Exception {
+        try {
+            return doRequest(simpleHttpclient, httpSendModel, getCharSet);
+        } finally {
+            simpleHttpclient.getHttpclient().getConnectionManager().shutdown();
+        }
 
-		HttpRequestBase httpRequest = buildHttpRequest(httpSendModel);
+    }
 
-		if (httpSendModel.getUrl().startsWith("https://")) {
-			simpleHttpclient.enableSSL();
-		}
+    /**
+     * @param httpSendModel
+     * @param getCharSet
+     * @return
+     * @throws Exception
+     */
+    public static SimpleHttpResponse doRequest(
+            SimpleHttpClient simpleHttpclient, HttpSendModel httpSendModel,
+            String getCharSet) throws Exception {
 
-		try {
-			HttpResponse response = simpleHttpclient.getHttpclient().execute(
-					httpRequest);
-			int statusCode = response.getStatusLine().getStatusCode();
+        HttpRequestBase httpRequest = buildHttpRequest(httpSendModel);
 
-			if (isRequestSuccess(statusCode)) {
-				return new SimpleHttpResponse(statusCode, EntityUtils.toString(
-						response.getEntity(), getCharSet), null);
-			} else {
-				return new SimpleHttpResponse(statusCode, null, response
-						.getStatusLine().getReasonPhrase());
-			}
+        if (httpSendModel.getUrl().startsWith("https://")) {
+            simpleHttpclient.enableSSL();
+        }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new Exception("http请求异常", e);
-		}
+        try {
+            HttpResponse response = simpleHttpclient.getHttpclient().execute(
+                    httpRequest);
+            int statusCode = response.getStatusLine().getStatusCode();
 
-	}
+            if (isRequestSuccess(statusCode)) {
+                return new SimpleHttpResponse(statusCode, EntityUtils.toString(
+                        response.getEntity(), getCharSet), null);
+            } else {
+                return new SimpleHttpResponse(statusCode, null, response
+                        .getStatusLine().getReasonPhrase());
+            }
 
-	/**
-	 * @param httpSendModel
-	 * @return
-	 * @throws Exception 
-	 */
-	protected static HttpRequestBase buildHttpRequest(
-			HttpSendModel httpSendModel) throws Exception {
-		HttpRequestBase httpRequest;
-		if (httpSendModel.getMethod() == null) {
-			throw new Exception("请求方式未设定");
-		} else if (httpSendModel.getMethod() == HttpMethod.POST) {
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("http请求异常", e);
+        }
 
-			String url = httpSendModel.getUrl();
-			String sendCharSet = httpSendModel.getCharSet();
-			List<HttpFormParameter> params = httpSendModel.getParams();
-			List<NameValuePair> qparams = new ArrayList<NameValuePair>();
-			if (params != null && params.size() != 0) {
+    }
 
-				for (HttpFormParameter param : params) {
-					qparams.add(new BasicNameValuePair(param.getName(), param
-							.getValue()));
-				}
+    /**
+     * @param httpSendModel
+     * @return
+     * @throws Exception
+     */
+    protected static HttpRequestBase buildHttpRequest(
+            HttpSendModel httpSendModel) throws Exception {
+        HttpRequestBase httpRequest;
+        if (httpSendModel.getMethod() == null) {
+            throw new Exception("请求方式未设定");
+        } else if (httpSendModel.getMethod() == HttpMethod.POST) {
 
-			}
+            String url = httpSendModel.getUrl();
+            String sendCharSet = httpSendModel.getCharSet();
+            List<HttpFormParameter> params = httpSendModel.getParams();
+            List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+            if (params != null && params.size() != 0) {
 
-			HttpPost httppost = new HttpPost(url);
-			try {
-				httppost.setEntity(new UrlEncodedFormEntity(qparams,
-						sendCharSet));
-			} catch (UnsupportedEncodingException e) {
-				throw new Exception("构建post请求参数失败", e);
-			}
+                for (HttpFormParameter param : params) {
+                    qparams.add(new BasicNameValuePair(param.getName(), param
+                            .getValue()));
+                }
 
-			httpRequest = httppost;
-		} else if (httpSendModel.getMethod() == HttpMethod.GET) {
-			HttpGet httpget = new HttpGet(httpSendModel.buildGetRequestUrl());
+            }
 
-			httpRequest = httpget;
-		} else {
-			throw new Exception("请求方式不支持：" + httpSendModel.getMethod());
-		}
+            HttpPost httppost = new HttpPost(url);
+            try {
+                httppost.setEntity(new UrlEncodedFormEntity(qparams,
+                        sendCharSet));
+            } catch (UnsupportedEncodingException e) {
+                throw new Exception("构建post请求参数失败", e);
+            }
 
-		return httpRequest;
-	}
+            httpRequest = httppost;
+        } else if (httpSendModel.getMethod() == HttpMethod.GET) {
+            HttpGet httpget = new HttpGet(httpSendModel.buildGetRequestUrl());
 
-	/**
-	 * 请求是否成功
-	 * 
-	 * @param statusCode
-	 * @return
-	 */
-	public static boolean isRequestSuccess(int statusCode) {
-		return statusCode == 200;
-	}
-	
-	
-	
-	
+            httpRequest = httpget;
+        } else {
+            throw new Exception("请求方式不支持：" + httpSendModel.getMethod());
+        }
+
+        return httpRequest;
+    }
+
+    /**
+     * 请求是否成功
+     *
+     * @param statusCode
+     * @return
+     */
+    public static boolean isRequestSuccess(int statusCode) {
+        return statusCode == 200;
+    }
+
 }
